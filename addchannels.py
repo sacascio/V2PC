2032c35e473e2f03e5a4a23485c89765518d304c
sacascio <sacascio@cisco.com>
2018-10-22 16:35:40 -0400

    enhancement_v1
    adding more code

--- addchannels.py
+++ addchannels.py
@@ -7,12 +7,73 @@ import json
 from IPy import IP
 import requests
 import os
+import warnings
 
+warnings.filterwarnings("ignore")
+
+
+
+def gettoken(ip,un,pw):
+
+    url = "https://%s:8443/api/platform/login" % ip
+    headers={ 'Content-Type':'application/json' }
+    payload = { 'username' : un, "password" : pw}
+    try:
+        response = requests.post(url,data=json.dumps(payload), headers=headers, verify=False, timeout=10)
+    except:
+        print "Timeout getting to V2PC Master"
+        sys.exit(9)
+    
+    if response.status_code == 200:
+            return response.json()['token']
+    
+    else:
+            print "Could not get token for authentication, error code %s" % (response.status_code)
+            sys.exit(9)
+
+
+def getchannels(ip,token):     
+    url = "https://%s:8443/api/platform/do/v2/channelsources" % (ip)
+    headers = { 'Content-Type':'application/json', 'Authorization' : 'Bearer %s ' % token }
+    response = requests.get(url,headers=headers, verify=False, timeout=30)
+    
+    if response.status_code == 200:
+        return response.json()  
+    
+    else:
+        print "Could not get channel list"
+        sys.exit(9) 
+
+def getstrprofiles(ip,token): 
+    profvalues = {}
+        
+    url = "https://%s:8443/api/platform/do/v2/streamprofiles" % (ip)
+    headers = { 'Content-Type':'application/json', 'Authorization' : 'Bearer %s ' % token }
+    response = requests.get(url,headers=headers, verify=False, timeout=30)
+    
+    if response.status_code == 200:
+        for d in response.json():
+            abrname = d['properties']['publishName']
+            abrid   = d['name']
+            
+            if abrname in profvalues:
+                print "ERROR: ABR Profile names must be unique. Found multiple ABR Profiles named %s  Please correct names in V2PC" % pubname
+                sys.exit(9)
+            
+            else:
+                profvalues[abrname] = abrid
+        
+        return profvalues
+    
+    else:
+        print "Could not get streaming profiles"
+        sys.exit(9)       
+        
 def usage():
 
     print " The following parameters are required:\n\n \
     \
-    f:      Name of input file ( ex. %s -f file.csv )\n \
+        f:      Name of input file ( ex. %s -f file.csv )\n \
         d:      Domain name (ex. -d mos.hcvlny.cv.net)\n \
         h:      Help message\n\n \
         Input file format ChannelDescription,Callsign,SourceIP,SD or HD,MulticastIP" % (sys.argv[0])
@@ -24,99 +85,45 @@ def main(argv):
     
     if len(argv) == 0:
         usage()
-    
-    
-    fileerr = False
+        
+    username = "admin"
+    password = "default"
+  
+ 
 
     try:
-        opts,args = getopt.getopt(argv,"f:d:p:u:",["file=","domain=","profile","udpports"])
+        opts,args = getopt.getopt(argv,"i:a:f:s:t:h",["masterip=","file=","status=","help"])
     except getopt.GetoptError as err:
         print str(err)
         sys.exit(2)
     else:
         for opt,arg in opts:
-            if opt in ("-d","--domain"):
-                domain = arg
-          
-            if opt in ("-f", "--file"):
-                file = arg
-          
-            if opt in ("-p", "--profile"):
-                profile = arg
-         
-            if opt in ("-u", "--udpports"):
-                udpports = arg
-        
-        
-        try:
-            domain
-        except:
-             sys.argv[0] + "Domain not specified (-d|--domain)"
-             sys.exit(1) 
-        
-        try:
-            profile
-        except:
-            print sys.argv[0] + "Streaming profile file not specified (-p|--profile)"
-            sys.exit(1)
-            
-        try:
-            file
-        except:
-            print sys.argv[0] + "Input file not specified (-f|--file)"
-            sys.exit(1)
-            
-        try:
-            udpports
-        except:
-            print sys.argv[0] + "UDP Ports file not specified (-u|--udpports)"
-            sys.exit(1)
-                           
-        if not os.path.isfile(file):
-            print sys.argv[0] + " Input file %s passed to -f|--file NOT found" % file
-            sys.exit(1)
-                    
-        elif not os.path.isfile(profile):
-            print sys.argv[0] + " Profile file %s passed to -p|--profile NOT found" % file
-            sys.exit(1)
-                
-        elif not os.path.isfile(udpports):
-            print sys.argv[0] + " UDP port file %s passed to -u|--udpports NOT found" % file
-            sys.exit(1)    
-                    
-        else:
-            # ChannelDescription,Callsign,SourceIP,SD or HD,MulticastIP
-            with open (file) as data:
-                lines = data.read().splitlines()
-                        
-            for data in lines:
-                fieldnum = data.split(",")
-                if len(fieldnum) != 5:
-                    print ("Input file format does not have 5 fields.  %s configured. See line %s of input file") % (len(fieldnum),lines.index(data) + 1)
-                    fileerr = True
-                            
-                try:
-                    IP(fieldnum[2])
-                except:
-                    print ("Source IP address %s is not a valid IP address.  See line %s of input file" ) % (fieldnum[2],lines.index(data) + 1)
-                    fileerr = True
-                            
-                try:
-                    IP(fieldnum[4])
-                except:
-                    print ("Multicast IP address %s is not a valid IP address.  See line %s of input file" ) % (fieldnum[4],lines.index(data) + 1)
-                    fileerr = True
-                            
-                if fieldnum[3].upper() != 'SD' and fieldnum[3].upper() != 'HD':
-                    print ("SD or HD must be specified after the source IP.  %s specified.  See line %s of input file" ) % (fieldnum[3],lines.index(data) + 1)
-                    fileerr = True
-                                
-            # Remove duplicates, if any
-            lines = list(set(lines))
-                    
-                    
-            if opt == '-h':
+            if opt in ("-h","--help"):
                 usage()
+            if opt in ("-i","--masterip"):
+                ip = arg
+        
+
+    try:
+        ip
+    except:
+        print "Required paramter -i|--masterip not passed"
+        sys.exit(9)
+  
+    
+                                
+    # Remove duplicates, if any
+    #lines = list(set(lines))
+                    
+                    
+    if opt == '-h':
+        usage()
+        
+    token = gettoken(ip,username,password)
+    #print json.dumps(getchannels(ip,token),indent=2)
+    print getstrprofiles(ip,token)
+    
+    sys.exit(9)
     
 if __name__ == '__main__':
     main(sys.argv[1:])
